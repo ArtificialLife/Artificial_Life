c10830d011648d88728576f9f4526faa31ae6798
Neill <neillro24124@gmail.com>
2015-03-24 23:20:05 -0500

    Gitignore update

---
+++ Sierpinski/src/TextProcessor.java
@@ -0,0 +1,34 @@
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+/**
+ * Created by Neill on 3/24/2015.
+ */
+public class TextProcessor {
+
+    public static void main(String[] args){
+        Scanner lectura = new Scanner(System.in);
+        String line = lectura.nextLine();
+        String entrada = line;
+        while(!line.equals("FIN")){
+            line = lectura.nextLine();
+            entrada = entrada + " "+ line;
+        }
+        entrada = entrada.toLowerCase();
+        entrada = entrada.replaceAll("í","i").replaceAll("ó","o").replaceAll("ú","u").replaceAll("á","a").replaceAll("é","e");
+        String[] words = entrada.split(" ");
+        Map<String, Integer> ranking = new TreeMap<String, Integer>();
+        for(int i=0;i< words.length;i++) {
+            if (ranking.containsKey(words[i])) {
+                ranking.put(words[i], 1 + ranking.get(words[i]));
+            } else {
+                ranking.put(words[i], 1);
+            }
+        }
+        for(String j: ranking.keySet()){
+            System.out.println(j+" : "+ranking.get(j));
+        }
+
+    }
+}
